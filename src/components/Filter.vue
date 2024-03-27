003b1e445404d041a67addc032fa5660c3a29487
Volkan <v.ocakli@gmail.com>
2024-03-27 19:49:58 +0300

    filter options added

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -7,6 +7,7 @@
       <h3>Name or Number</h3>
       <v-card-text>
         <v-text-field
+          placeholder="Search for a Pokemon by name or using its Pokedex number."
           variant="outlined"
           v-model="this.searchQuery"
           append-inner-icon="mdi-magnify"
@@ -19,14 +20,10 @@
         Use the Advanced Search to explore Pokémon by type,size and more!
       </p>
     </div>
-    <!--Green Text-->
-    <div class="bg-green w-lg-25 w-sm-100 py-6 my-6 rounded-lg">
-      <p>Search for a Pokemon by name or using its National Pokedex number.</p>
-    </div>
   </div>
 
   <div class="filter-container" v-if="filterSection">
-    <div class="d-flex align-center">
+    <div class="d-flex justify-space-between">
       <v-chip-group class="d-flex flex-wrap w-25" multiple filter>
         <v-chip
           v-for="(type, index) in orderedTypes"
@@ -41,6 +38,61 @@
           </p>
         </v-chip>
       </v-chip-group>
+
+      <div class="size-container d-flex flex-column justify-space-around w-50">
+        <div>
+          <h3 class="size-title">Height</h3>
+          <ul class="size-list">
+            <li
+              class="icon"
+              @click="selectSize('height_small')"
+              :class="{ 'icon-highlight': isSizeSelected('height_small') }"
+            >
+              <span class="icons icon_filter_height_small"></span>
+            </li>
+            <li
+              class="icon"
+              @click="selectSize('height_medium')"
+              :class="{ 'icon-highlight': isSizeSelected('height_medium') }"
+            >
+              <span class="icons icon_filter_height_medium"></span>
+            </li>
+            <li
+              class="icon"
+              @click="selectSize('height_large')"
+              :class="{ 'icon-highlight': isSizeSelected('height_large') }"
+            >
+              <span class="icons icon_filter_height_large"></span>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 class="size-title">Weight</h3>
+          <ul class="size-list">
+            <li
+              class="icon"
+              @click="selectSize('weight_small')"
+              :class="{ 'icon-highlight': isSizeSelected('weight_small') }"
+            >
+              <span class="icons icon_filter_weight_small"></span>
+            </li>
+            <li
+              class="icon"
+              @click="selectSize('weight_medium')"
+              :class="{ 'icon-highlight': isSizeSelected('weight_medium') }"
+            >
+              <span class="icons icon_filter_weight_medium"></span>
+            </li>
+            <li
+              class="icon"
+              @click="selectSize('weight_large')"
+              :class="{ 'icon-highlight': isSizeSelected('weight_large') }"
+            >
+              <span class="icons icon_filter_weight_large"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     <v-btn
       prepend-icon="mdi-magnify"
@@ -67,10 +119,11 @@ export default {
   data() {
     return {
       types: [],
-      filterSection: false,
+      filterSection: true,
       selectedType: [],
       filteredPokemons: [],
       searchQuery: "",
+      selectedSizes: [],
     };
   },
   methods: {
@@ -91,8 +144,12 @@ export default {
     },
     filterByType() {
       const filterTypes = this.selectedType.map((type) => type.name);
+      const filterSizes = this.selectedSizes.map((size) => size);
+
       this.filteredPokemons = this.pokemons.filter((pokemon) => {
-        if (
+        if (filterSizes.includes("height_small") && pokemon.height < 3) {
+          return this.filteredPokemons;
+        } else if (
           (filterTypes.length == 2 &&
             filterTypes.every((value) => pokemon.types.includes(value))) ||
           filterTypes == pokemon.types[0] ||
@@ -103,6 +160,8 @@ export default {
         }
       });
       this.$emit("filterEvent", this.filteredPokemons);
+      console.log(filterSizes);
+      console.log(filterTypes);
     },
     performSearch() {
       this.filteredPokemons = this.pokemons.filter(
@@ -111,6 +170,18 @@ export default {
           pokemon.id.toString().includes(this.searchQuery)
       );
       this.$emit("searchEvent", this.filteredPokemons);
+    },
+    selectSize(size) {
+      const index = this.selectedSizes.indexOf(size);
+      if (index === -1) {
+        this.selectedSizes.push(size);
+      } else {
+        this.selectedSizes.splice(index, 1);
+      }
+      console.log(this.selectedSizes);
+    },
+    isSizeSelected(size) {
+      return this.selectedSizes.includes(size);
     },
   },
   computed: {
@@ -125,6 +196,15 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: "icons";
+  src: url("../assets/icons.woff") format("woff");
+  /* Additional font-face properties */
+}
+.icons {
+  font-family: "icons", sans-serif;
+}
+
 .filter-container {
   background-color: #616161;
   margin: auto;
@@ -139,6 +219,63 @@ h3 {
 }
 .subtitle {
   color: #fff;
+}
+
+.icon_filter_height_large:before {
+  content: "\f116";
+}
+
+.icon_filter_height_medium:before {
+  content: "\f117";
+}
+
+.icon_filter_height_small:before {
+  content: "\f118";
+}
+
+.icon_filter_weight_large:before {
+  content: "\f119";
+}
+
+.icon_filter_weight_medium:before {
+  content: "\f11a";
+}
+
+.icon_filter_weight_small:before {
+  content: "\f11b";
+}
+
+.icon {
+  float: left;
+  width: 27.46%;
+  background-color: #f2f2f2;
+  border-radius: 12px;
+  color: #313131;
+  cursor: pointer;
+  height: 100%;
+  margin: 0 2%;
+  overflow: hidden;
+}
+
+.size-list span {
+  font-size: 300%;
+  text-align: center;
+  line-height: 80px;
+  width: 100%;
+}
+.size-list span.icon_filter_height_large {
+  font-size: 500%;
+}
+
+.size-title {
+  color: #f2f2f2;
+  text-align: left;
+  margin: 0 2%;
+  cursor: default;
+}
+.icon-highlight {
+  background-color: #ee6b2f;
+  color: #f2f2f2;
 }
 
 @media only screen and (max-width: 600px) {
